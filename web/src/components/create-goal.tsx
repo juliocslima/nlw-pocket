fd0776c583cef7499f992df7e63feae650ffb834
Julio Lima <lima.julio.cs@gmail.com>
2024-09-11 19:22:55 -0300

    class - day 3

--- web/src/components/create-goal.tsx
+++ web/src/components/create-goal.tsx
@@ -13,8 +13,44 @@ import {
   RadioGroupItem,
 } from './ui/radio-group'
 import { Button } from './ui/button'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createGoal } from '../http/create-goal'
+import { useQueryClient } from '@tanstack/react-query'
+
+const createGoalSchema = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalSchema = z.infer<typeof createGoalSchema>
 
 export function CreateGoal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<CreateGoalSchema>({
+    resolver: zodResolver(createGoalSchema),
+  })
+
+  const queryClient = useQueryClient()
+
+  async function handleCreateGoal(data: CreateGoalSchema) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    })
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+
+    reset()
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
@@ -28,11 +64,14 @@ export function CreateGoal() {
 
           <DialogDescription>
             Adicione atividades que te fazem bem e que você quer continuar
-            DialogDescriptionraticando toda semana.
+            praticando toda semana.
           </DialogDescription>
         </div>
 
-        <form action="" className="flex-1 flex flex-col justify-center">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-center"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <Label>Qual a atividade?</Label>
@@ -40,60 +79,79 @@ export function CreateGoal() {
                 id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar, etc..."
+                {...register('title')}
               />
+
+              {errors.title && (
+                <p className="text-red-400 text-sm">{errors.title.message}</p>
+              )}
             </div>
 
             <div className="flex flex-col gap-2">
               <Label>Quantas vezes na semana?</Label>
-              <RadioGroup>
-                <RadioGroupItem value="1">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    1x na semana
-                  </span>
-                  <span className="text-lg leading-none">🥱</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="2">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    2x na semana
-                  </span>
-                  <span className="text-lg leading-none">🙂</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="3">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    3x na semana
-                  </span>
-                  <span className="text-lg leading-none">😎</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="4">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    4x na semana
-                  </span>
-                  <span className="text-lg leading-none">😜</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="5">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    5x na semana
-                  </span>
-                  <span className="text-lg leading-none">🤨</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="6">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    6x na semana
-                  </span>
-                  <span className="text-lg leading-none">🤯</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="7">
-                  <RadioGroupIndicator />
-                  <span>Todos os dias na semana</span>
-                  <span>🔥</span>
-                </RadioGroupItem>
-              </RadioGroup>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={3}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      <RadioGroupItem value="1">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          1x na semana
+                        </span>
+                        <span className="text-lg leading-none">🥱</span>
+                      </RadioGroupItem>
+                      <RadioGroupItem value="2">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          2x na semana
+                        </span>
+                        <span className="text-lg leading-none">🙂</span>
+                      </RadioGroupItem>
+                      <RadioGroupItem value="3">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          3x na semana
+                        </span>
+                        <span className="text-lg leading-none">😎</span>
+                      </RadioGroupItem>
+                      <RadioGroupItem value="4">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          4x na semana
+                        </span>
+                        <span className="text-lg leading-none">😜</span>
+                      </RadioGroupItem>
+                      <RadioGroupItem value="5">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          5x na semana
+                        </span>
+                        <span className="text-lg leading-none">🤨</span>
+                      </RadioGroupItem>
+                      <RadioGroupItem value="6">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          6x na semana
+                        </span>
+                        <span className="text-lg leading-none">🤯</span>
+                      </RadioGroupItem>
+                      <RadioGroupItem value="7">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          Todos os dias na semana
+                        </span>
+                        <span className="text-lg leading-none">🔥</span>
+                      </RadioGroupItem>
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
 
             <div className="flex items-center gap-3">
@@ -102,9 +160,7 @@ export function CreateGoal() {
                   Fechar
                 </Button>
               </DialogClose>
-              <Button type="button" className="flex-1">
-                Salvar
-              </Button>
+              <Button className="flex-1">Salvar</Button>
             </div>
           </div>
         </form>
